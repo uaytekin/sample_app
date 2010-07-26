e4b2b3257778ea557708a1a9c0d558776b0d195d
uaytekin <umut.aytekin@gmail.com>
2010-07-25 22:22:14 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,8 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.signup '/signup',   :controller => 'users', :action => 'new'
+
 
 
 end
